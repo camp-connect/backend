fcb4423b32b20c6fbec78730461aa081dfdcb5a0
namishk <namishkhandelwal1@gmail.com>
2022-04-13 11:26:41 +0530

    login and signup api

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/api/login", (req, res) =>  {
-    var userEmail = req.body.userEmail;
-    var userPassword = req.body.userPassword;
-    const chkUser = () =>{
-        if(userEmail === 'testuser@jklu.edu.in' & userPassword === "rootAccess"){
-            res.status(200).json({UserEmail:userEmail, redirect:true});
-        }
-        else{
-            res.status(500).json({UserEmail:null , redirect:false});
-        }
-    }
-    
-    chkUser();
-})
 
+// router.get("/api/login", (req, res) =>  {
+//     var userEmail = req.body.userEmail;
+//     var userPassword = req.body.userPassword;
+//     const chkUser = () =>{
+//         if(userEmail === 'testuser@jklu.edu.in' & userPassword === "rootAccess"){
+//             res.status(200).json({UserEmail:userEmail, redirect:true});
+//         }
+//         else{
+//             res.status(500).json({UserEmail:null , redirect:false});
+//         }
+//     }
+    
+//     chkUser();
+// })
+
+// router.get("/api/signup", (req, res) => {
+//     var userEmail = req.body.userEmail;
+//     var userPassword = req.body.userPassword;    
+// })
 module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require('express');
-// const { auth } = require('express-openid-connect');
 var indexrouter = require("./routes/index.js");
 const bodyParser = require('body-parser')
-
 require('dotenv').config();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    user: "iyhkixuy",
+    host: "rosie.db.elephantsql.com",
+    database: "iyhkixuy",
+    password: "a6sU_mXJolUT9BKKc6K5hW5MWUyUjV3g",
+    port: 5432
+  });
+  
+
 const app = express();
 
 var port = process.env.PORT || 3000;
-
-// const config = {
-//     authRequired: false,
-//     auth0Logout: true,
-//     secret: process.env.SECRET,
-//     baseURL: process.env.BASEURL,
-//     clientID: process.env.CLIENTID,
-//     issuerBaseURL: process.env.ISSUERBASEURL,
-
-//   };
 
 //middleware
 app.use(bodyParser.json());
@@ -24,5 +23,70 @@ app.use(bodyParser.json());
 
 app.use("/", indexrouter);
 
+//database
 
-app.listen(port);
+const student = `CREATE TABLE IF NOT EXISTS students(
+    name varchar(50) NOT NULL,
+    roll char(12) PRIMARY KEY,
+    roomNo varchar(3) NOT NULL,
+    hostel varchar(10) NOT NULL,
+    password varchar(50) NOT NULL,
+    email varchar(30));
+`;
+
+pool.query(student, [], (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log("Successful creation of the 'student' table");
+  });
+  
+
+//APIs
+
+app.get("/api/signup", (req, res) => {
+    var userEmail = req.body.userEmail;
+    var userPassword = req.body.userPassword;
+    var hostel = req.body.hostel;
+    var name = req.body.name;
+    var roll = req.body.roll;
+    var room = req.body.room;
+
+    const ins = `insert into students (name, roll, roomNo, hostel, password, email) values('${name}', '${roll}', '${room}', '${hostel}', '${userPassword}', '${userEmail}');`;
+    pool.query(ins, [], (err, result) =>{
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({status: false});
+        }
+        else{
+            res.status(200).json({status: true});
+        }
+        
+    })
+
+})
+
+app.get("/api/login", (req, res) =>  {
+    var userEmail = req.body.userEmail;
+    var userPassword = req.body.userPassword;
+    const chkUser = () =>{
+        const q = `select email, password from students where email = '${userEmail}' and password = '${userPassword}';`
+        pool.query(q, [], (err, result) =>{
+            if(err){
+                console.error(err.message);
+                res.status(500);
+            }
+            else{
+                if(result.rowCount != 0){
+                    res.status(200).json(result.rows[0]);
+                }
+                else{
+                    res.status(300);
+                }
+            }
+        })
+    }
+    
+    chkUser();
+})
+app.listen(port);       
